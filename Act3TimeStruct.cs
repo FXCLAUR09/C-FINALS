a8752e4f3d2b78aebbcf829a59d630b09366fd98
John Laurence E. Aballe <jlaurenceaballe1823@gmail.com>
2025-06-12 22:49:11 +0800

    Activity 6

--- Act3TimeStruct.cs
+++ Act3TimeStruct.cs
@@ -9,21 +9,19 @@ struct Time
         minutes = 60 * hh + mm;
     }
 
-    public int Hour
+    public Time(int totalMinutes)
     {
-        get { return minutes / 60; }
+        minutes = totalMinutes;
     }
 
-    public int Minute
-    {
-        get { return minutes % 60; }
-    }
+    public int Hour => minutes / 60;
+    public int Minute => minutes % 60;
 
     public override string ToString()
     {
         return String.Format("{0:D2}:{1:D2}", Hour, Minute);
     }
-    
+
     public static Time operator +(Time t1, Time t2)
     {
         return new Time(t1.minutes + t2.minutes);
@@ -33,22 +31,29 @@ struct Time
     {
         return new Time(t1.minutes - t2.minutes);
     }
-   
-}
 
+    public static implicit operator Time(int totalMinutes)
+    {
+        return new Time(totalMinutes);
+    }
+
+    public static explicit operator int(Time t)
+    {
+        return t.minutes;
+    }
+
+}
+   
 class Program
 {
     static void Main()
     {
-        Time t1 = new Time(2, 30);      // 02:30
-        Time t2 = new Time(1, 45);      // 01:45
+        Time t1 = new Time(1, 30);      
+        Time t2 = 90;                   
+        int mins = (int)t1;            
 
-        Time sum = t1 + t2;             // 04:15
-        Time diff = t1 - t2;            // 00:45
-
-        Console.WriteLine("t1: " + t1);        // 02:30
-        Console.WriteLine("t2: " + t2);        // 01:45
-        Console.WriteLine("Sum: " + sum);      // 04:15
-        Console.WriteLine("Diff: " + diff);    // 00:45
+        Console.WriteLine("t1: " + t1);        
+        Console.WriteLine("t2 (from int): " + t2);  
+        Console.WriteLine("Minutes in t1: " + mins); 
     }
 }
