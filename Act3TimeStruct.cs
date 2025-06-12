0f73799f7419fd7266cc99b236f6c52bf8e2e2f1
John Laurence E. Aballe <jlaurenceaballe1823@gmail.com>
2025-06-12 22:36:37 +0800

    Activity 4

--- Act3TimeStruct.cs
+++ Act3TimeStruct.cs
@@ -9,9 +9,19 @@ struct Time
         minutes = 60 * hh + mm;
     }
 
+    public int Hour
+    {
+        get { return minutes / 60; }
+    }
+
+    public int Minute
+    {
+        get { return minutes % 60; }
+    }
+
     public override string ToString()
     {
-        return $"{minutes} minutes past midnight";
+        return String.Format("{0:D2}:{1:D2}", Hour, Minute);
     }
    
 }
@@ -20,10 +30,15 @@ class Program
 {
     static void Main()
     {
-        Time t1 = new Time(10, 5);  // 10:05
-        Time t2 = new Time(0, 45);  // 00:45
+        Time t1 = new Time(10, 5);    // 10:05
+        Time t2 = new Time(23, 45);   // 23:45
+        Time t3 = new Time(0, 9);     // 00:09
 
-        Console.WriteLine(t1);  // Output: 605
-        Console.WriteLine(t2);  // Output: 45
+        Console.WriteLine(t1);  // Output: 10:05
+        Console.WriteLine(t2);  // Output: 23:45
+        Console.WriteLine(t3);  // Output: 00:09
+        
+        Console.WriteLine("Hour: " + t2.Hour);   // 23
+        Console.WriteLine("Minute: " + t2.Minute); // 45
     }
 }
