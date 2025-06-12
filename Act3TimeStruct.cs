dcc4c295af58fe50eac4c2732525cea1ef74c8cf
John Laurence E. Aballe <jlaurenceaballe1823@gmail.com>
2025-06-12 22:42:47 +0800

    Activity 5

--- Act3TimeStruct.cs
+++ Act3TimeStruct.cs
@@ -23,6 +23,16 @@ struct Time
     {
         return String.Format("{0:D2}:{1:D2}", Hour, Minute);
     }
+    
+    public static Time operator +(Time t1, Time t2)
+    {
+        return new Time(t1.minutes + t2.minutes);
+    }
+
+    public static Time operator -(Time t1, Time t2)
+    {
+        return new Time(t1.minutes - t2.minutes);
+    }
    
 }
 
@@ -30,15 +40,15 @@ class Program
 {
     static void Main()
     {
-        Time t1 = new Time(10, 5);    // 10:05
-        Time t2 = new Time(23, 45);   // 23:45
-        Time t3 = new Time(0, 9);     // 00:09
+        Time t1 = new Time(2, 30);      // 02:30
+        Time t2 = new Time(1, 45);      // 01:45
 
-        Console.WriteLine(t1);  // Output: 10:05
-        Console.WriteLine(t2);  // Output: 23:45
-        Console.WriteLine(t3);  // Output: 00:09
-        
-        Console.WriteLine("Hour: " + t2.Hour);   // 23
-        Console.WriteLine("Minute: " + t2.Minute); // 45
+        Time sum = t1 + t2;             // 04:15
+        Time diff = t1 - t2;            // 00:45
+
+        Console.WriteLine("t1: " + t1);        // 02:30
+        Console.WriteLine("t2: " + t2);        // 01:45
+        Console.WriteLine("Sum: " + sum);      // 04:15
+        Console.WriteLine("Diff: " + diff);    // 00:45
     }
 }
